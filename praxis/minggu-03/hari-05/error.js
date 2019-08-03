d195a6736762274268929c80cae70b8df07d276f
Bayu Samudra <bayu.samudra@hotmail.com>
2019-08-03 14:06:50 +0700

    tambah error

---
+++ praxis/minggu-03/hari-05/error.js
@@ -0,0 +1,11 @@
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk");
+  }
+}
+
+console.log(primitiveMultiply(8, 8));
